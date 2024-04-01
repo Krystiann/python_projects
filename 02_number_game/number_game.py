fd7ead094e483834998371eacceea8feaddc374a
Krystiann <Krystiann.Siudut@gmail.com>
2024-04-01 09:28:38 +0200

    Update number_game.py

--- 02_number_game/number_game.py
+++ 02_number_game/number_game.py
@@ -1,1 +1,22 @@
+from random import randint
+
+random_number: int = randint(0, 20)
+
+while True:
+    try:
+        user_input: int = int(input('Enter a number from 0 to 20 : '))
+    except ValueError:
+        print('Invalid number')
+        continue
+
+    if user_input > random_number:
+        print('Too big a number !')
+
+    elif user_input < random_number:
+        print('Too small a number !')
+
+    else:
+        print('You won !')
+        break
+
 
